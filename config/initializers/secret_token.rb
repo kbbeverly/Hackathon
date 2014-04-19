512d4f791f069b4d4a420498ab0f6b7f16577311
kbbeverly <kbbeverly@valdosta.edu>
2014-04-19 13:01:13 -0400

    Starting Hackathon Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hackathon::Application.config.secret_key_base = 'bc23c9d107eb19b238fbfa70f7bae0bb8b14d11413a592e0591638d99c1cafc2bdb5786aaa1a79f251fc6eb69ce787dc4c1628dbcbb5cc4b142a48961901835c'
